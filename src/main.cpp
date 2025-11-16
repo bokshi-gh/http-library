a08565decaa43e7932c540ade6be2060e50372d8
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-16 11:14:19 +0545

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include "http_codec.hpp"
 #include "http_library.hpp"
+#include <iostream>
 #include <string>
 
 int main() {
     Server srv;
-    srv.get("/", [&](HTTPRequest request, HTTPResponse response) {
+
+    srv.get("/", [&](HTTPRequest& request, HTTPResponse& response) {
         response.headers["Content-Type"] = "text/html";
         response.body = "<p>a monk in a cloud</p>";
     });
+
     srv.listen(8080, [&]() {
-        std::cout << "server is listening on port 8080" << "\n";
+        std::cout << "Server is listening on port 8080\n";
     });
+
     return 0;
 }
