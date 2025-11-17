e6b4c6bf79dbdae2bb3270faa4a144e7d25513fa
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-17 19:13:55 +0545

    Update http_library.hpp

--- include/http_library.hpp
+++ include/http_library.hpp
@@ -11,7 +11,7 @@ class Server {
 private:
     int server_fd;
     uint16_t port;
-    unordered_map<string, function<void(HTTPRequest&, HTTPResponse&)>> route_table;
+    unordered_map<string, function<void(HTTPRequest&, HTTPResponse&)>> endpoint_table;
 
     void handle_client(int client_fd);
 
@@ -19,9 +19,8 @@ public:
     Server();
     ~Server();
 
-    void get(string route, function<void(HTTPRequest&, HTTPResponse&)> route_handler);
-
-    void listen(uint16_t port, function<void()> callback = nullptr); // callback parameter becomes nullptr if second argument is not passed when calling the listen function
+    void get(string route, function<void(HTTPRequest&, HTTPResponse&)> endpoint_handler);
+    void listen(uint16_t port, function<void()> callback = nullptr);
 };
 
 class Client {
