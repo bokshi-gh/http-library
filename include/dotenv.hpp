ebc6731ff9f3a2b1d9d14d65bd5c5313eeb4ce1a
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-17 13:19:59 +0545

    Add files via upload

---
+++ include/dotenv.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+namespace dotenv {
+
+    static std::unordered_map<std::string, std::string> envmap;
+
+    inline std::string trim(std::string s) {
+        s.erase(0, s.find_first_not_of(" \t\r\n"));
+        s.erase(s.find_last_not_of(" \t\r\n") + 1);
+        return s;
+    }
+
+    inline void init(const char* path = nullptr) {
+        const char* filepath = path ? path : "./.env";
+
+        std::ifstream file(filepath);
+        if (!file.is_open()) return;
+
+        std::string line;
+        while (std::getline(file, line)) {
+            if (line.empty() || line[0] == '#') continue;
+
+            auto pos = line.find('=');
+            if (pos == std::string::npos) continue;
+
+            std::string key = trim(line.substr(0, pos));
+            std::string value = trim(line.substr(pos + 1));
+
+            // remove quotes
+            if ((value.front() == '"' && value.back() == '"') ||
+                (value.front() == '\'' && value.back() == '\'')) {
+                value = value.substr(1, value.size() - 2);
+            }
+
+            envmap[key] = value;
+        }
+    }
+
+    inline std::string getenv(const std::string& key) {
+        if (envmap.find(key) != envmap.end())
+            return envmap[key];
+        return "";
+    }
+}
+
