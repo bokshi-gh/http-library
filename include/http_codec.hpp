5a1bd45d3d8c39d2da33b5579cdd5da844d84288
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-17 23:33:17 +0545

    Add files via upload

---
+++ include/http_codec.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <cstdint>
+
+using namespace std;
+
+class HTTPRequest {
+public:
+    string raw;
+    string method;
+    string path;
+    string version;
+    unordered_map<string, string> headers;
+    string body;
+};
+
+class HTTPResponse {
+public:
+    string raw;
+    string version;
+    uint16_t status_code;
+    string reason_phrase;
+    unordered_map<string, string> headers;
+    string body;
+};
+
+HTTPRequest decode_http_request(const char *raw_request);
+HTTPResponse decode_http_response(const char *raw_response);
+
+string encode_http_request(const HTTPRequest &request);
+string encode_http_response(const HTTPResponse &response);
