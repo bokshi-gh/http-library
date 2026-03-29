0b06bf5098127349d0904dfac7cbbcbe1ab50d0c
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-03-29 15:20:16 +0545

    Update router_helpers.cpp

--- src/router_helpers.cpp
+++ src/router_helpers.cpp
@@ -15,3 +15,13 @@ bool match_route(const std::string& route_path, const std::string& request_path,
 
     return route_stream.eof() && request_stream.eof();
 }
+
+std::string get_current_date() {
+    time_t now = std::time(nullptr);
+    tm gm_time{};
+    gmtime_r(&now, &gm_time);
+
+    char buf[30];
+    strftime(buf, sizeof(buf), "%a, %d %b %Y %H:%M:%S GMT", &gm_time);
+    return std::string(buf);
+}
