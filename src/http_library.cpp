788d039747da52e4427535209fd929276ee2cfff
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-03-29 20:22:22 +0545

    Update http_library.cpp

--- src/http_library.cpp
+++ src/http_library.cpp
@@ -6,8 +6,20 @@ HTTPServer::~HTTPServer() {
     if (server_fd >= 0) close(server_fd);
 }
 
-void HTTPServer::get(const string& path, RouteHandler route_handler) {
+void HTTPServer::http_get(const string& path, RouteHandler route_handler) {
     router.add("GET", path, route_handler);
+}
+
+void HTTPServer::http_post(const string& path, RouteHandler route_handler) {
+    router.add("POST", path, route_handler);
+}
+
+void HTTPServer::http_put(const string& path, RouteHandler route_handler) {
+    router.add("PUT", path, route_handler);
+}
+
+void HTTPServer::http_delete(const string& path, RouteHandler route_handler) {
+    router.add("DELETE", path, route_handler);
 }
 
 void HTTPServer::listen(uint16_t port, function<void()> callback) {
