155f044e5333aba8db3f944c75fc93cbf8056cd8
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-03-29 15:22:12 +0545

    Update router_helpers.hpp

--- include/router_helpers.hpp
+++ include/router_helpers.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include "http_codec.hpp"
+
 #include <string>
 #include <sstream>
-#include "http_codec.hpp"
+#include <ctime>
 
 // Returns true if the route_path matches the request_path
 // Fills request.parameters for dynamic segments like :id
 bool match_route(const std::string& route_path, const std::string& request_path, HTTPRequest& request);
+
+std::string get_current_date();
