6a5ca18625e9b24305b59280b2b0bde119a0dc6a
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-03-29 13:03:20 +0545

    impl get function

--- src/http_library.cpp
+++ src/http_library.cpp
@@ -1,1 +1,11 @@
 #include "http_library.hpp"
+
+void remove_trailing_forward_slash(string& str) {
+  if (str.size() > 1 && str.back() == '/') str.pop_back();
+}
+
+void Server::get(string path, function<void(HTTPRequest, HTTPResponse&)> route_handler) {
+  remove_trailing_forward_slash(path);
+
+  routing_table[path] = route_handler;
+}
