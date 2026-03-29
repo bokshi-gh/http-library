f13f317e108ffdbaada23fa07e2b89a55f5780d0
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-03-29 15:05:48 +0545

    Update router_helpers.cpp

--- src/router_helpers.cpp
+++ src/router_helpers.cpp
@@ -1,3 +1,16 @@
 #include "router_helpers.hpp"
 
+bool match_pattern(const std::string& pattern, const std::string& path, HTTPRequest& request) {
+    std::stringstream p(pattern), q(path);
+    std::string pp, qq;
 
+    while (getline(p, pp, '/') && getline(q, qq, '/')) {
+        if (!pp.empty() && pp[0] == ':') {
+            request.parameters[pp.substr(1)] = qq;
+        } else if (pp != qq) {
+            return false;
+        }
+    }
+
+    return p.eof() && q.eof();
+}
