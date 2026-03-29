13383d9439e4a76434f5e50d474cc4efc30a8f89
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-03-29 12:20:46 +0545

    update

--- include/http_library.hpp
+++ include/http_library.hpp
@@ -1,40 +1,6 @@
 #pragma once
 
 #include "http_codec.hpp"
-#include "dotenv.hpp"
-#include <cstdint>
-#include <unordered_map>
-#include <string>
-#include <functional>
+#include "dotenv_cpp.hpp"
 
 using namespace std;
-
-class Server {
-private:
-    int server_fd;
-    uint16_t port;
-    unordered_map<string, function<void(HTTPRequest&, HTTPResponse&)>> pattern_table;
-
-    bool match_pattern(const string pattern, const string path, HTTPRequest &request);
-    string getHTTPDate();
-    void handle_client(int client_fd);
-
-public:
-    Server();
-    ~Server();
-
-    void get(string pattern, function<void(HTTPRequest&, HTTPResponse&)> pattern_handler);
-    void listen(uint16_t port, function<void()> callback = nullptr);
-};
-
-class Client {
-private:
-	string hostname;
-	uint16_t port;
-
-public:
-	Client(string hostname);
-	Client(string hostname, uint16_t port);
-
-	HTTPResponse get(const string path, const unordered_map<string, string> headers = {});
-};
--- scripts/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GREEN='\033[0;32m'
-NC='\033[0m'
-
-echo -e "${GREEN}[CLEANING BUILD AND DIST DIRECTORIES]${NC}"
-rm -rfv build dist
-echo -e "${GREEN}[CLEAN COMPLETE]${NC}"
