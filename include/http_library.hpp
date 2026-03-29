bdf8262bd59c7054a6947a52e5030f45e0a15ffd
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-03-29 12:55:09 +0545

    add function defintaion in class

--- include/http_library.hpp
+++ include/http_library.hpp
@@ -3,8 +3,27 @@
 #include "http_codec.hpp"
 #include "dotenv_cpp.hpp"
 
+#include <unordered_map>
+
 using namespace std;
 
+struct Route {
+  string method;
+  string path;
+}
+
 class Server {
-  private
+  private:
+    int server_fd;
+    uint16_t port;
+
+    unordered_map<Route, function<void(HTTPRequest, HTTPResponse&)>> routing_table;
+
+  public:
+    Server();
+    ~Server();
+
+    void get(string path, function<void(HTTPRequest, HTTPResponse&)> route_handler);
+
+    void listen(uint16_t port, function<void()> callback = nullptr);
 }
