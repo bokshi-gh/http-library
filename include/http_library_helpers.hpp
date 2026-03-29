1f62d68dcd60d09b0d01b75f6ce4d4cae4ca420c
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-03-29 15:06:31 +0545

    Update http_library_helpers.hpp

--- include/http_library_helpers.hpp
+++ include/http_library_helpers.hpp
@@ -1,1 +1,11 @@
+#include "http_library_helpers.hpp"
 
+std::string get_current_date() {
+    time_t now = std::time(nullptr);
+    tm gm_time{};
+    gmtime_r(&now, &gm_time);
+
+    char buf[30];
+    strftime(buf, sizeof(buf), "%a, %d %b %Y %H:%M:%S GMT", &gm_time);
+    return std::string(buf);
+}
