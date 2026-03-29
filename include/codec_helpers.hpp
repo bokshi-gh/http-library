6727a48a2f056b5564385d225147300dffc0da8f
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-03-29 20:37:11 +0545

    add library

---
+++ include/codec_helpers.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "http_codec.hpp"
+#include <string>
+#include <regex>
+
+using namespace std;
+
+// Request
+void parse_request_target(HTTPRequest& req);
+void validate_request_target(const string& request_target);
+void normalize_request_target(HTTPRequest& req);
+void parse_request_line(HTTPRequest& req, const string& line);
+
+// Response
+void parse_response_line(HTTPResponse& res, const string& line);
+
+// Headers (generic for request/response)
+template<typename T>
+void parse_headers(T& msg, const string& headers_block);
