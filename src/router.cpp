92c6465dc70406ff90a9dc31872ec24b0e3bbf3d
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-03-30 18:23:30 +0545

    update

--- src/router.cpp
+++ src/router.cpp
@@ -2,7 +2,7 @@
 #include "router_helpers.hpp"
 #include <iostream>
 
-void Router::add(const string& method, const string& path, RouteHandler route_handler) {
+void Router::add_entry_to_routing_table(const string& method, const string& path, RouteHandler route_handler) {
     validate_path(path);
     string normalized_path = normalize_path(path);
     routing_table[{method, normalized_path}] = route_handler;
