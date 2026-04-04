b2e5f9fc9bfc874a18219a4804d9bf868cc91215
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-04-04 14:04:55 +0545

    Update router.cpp

--- src/http-server/router/router.cpp
+++ src/http-server/router/router.cpp
@@ -4,7 +4,6 @@
 
 void Router::register_handler(const string& method, const string& path, RouteHandler route_handler) {
     validate_path(path);
-    string normalized_path = normalize_path(path);
     routing_table[{method, normalized_path}] = route_handler;
 }
 
